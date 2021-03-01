8c6a95ce5b0b724e12f56b625dd77576b4f168ec
CHAMPMOON <antipov8v@gmail.com>
2021-03-01 20:30:32 +0300

    lab_2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,20 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	int len, i, j;
+    char temp;
+    len = 0;
+    i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+        len++;
+    }
+    for (i = 0, j = len - 1;i <= j; i++, j--)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
 }
 
